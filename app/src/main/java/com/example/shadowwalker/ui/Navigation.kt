89a2709129416058b819e1a5d97fd71575dae31a
rahulp <rahulp5875@gmail.com>
2025-02-21 16:30:25 +0530

    Navigation work in progress.

---
+++ app/src/main/java/com/example/shadowwalker/ui/Navigation.kt
@@ -0,0 +1,12 @@
+package com.example.shadowwalker.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation(
+    tabNavController: NavHostController = rememberNavController()
+){
+
+}
